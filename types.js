00196c89fb4fead7a6c33a4c2b684f61143679e4
osidekyle <hawkinskyle67@gmail.com>
2020-10-09 11:55:49 -0700

    learned about variable types and started parameter and return types

---
+++ types.js
@@ -0,0 +1,25 @@
+var myString;
+var myNum;
+var myBool;
+var myVar;
+/*
+let strArr:string[];
+let numArr:number[];
+let boolArr:boolean[];
+*/
+var strArr;
+var numArr;
+var boolArr;
+var strNumTuple;
+myString = "Hello TS";
+myNum = 2.22;
+myBool = true;
+myVar = "yuh";
+strNumTuple = ["hello", 4];
+strArr = ["Hello", "World"];
+numArr = [1, 2, 3];
+boolArr = [true, false];
+var myVoid = null;
+var myNull = null;
+var myUndefined = undefined;
+console.log(myVoid);
---
+++ types.ts
@@ -0,0 +1,43 @@
+let myString: string;
+let myNum: number;
+let myBool:boolean;
+let myVar:any;
+
+/*
+let strArr:string[];
+let numArr:number[];
+let boolArr:boolean[];
+*/
+
+let strArr:Array<string>
+let numArr:Array<number>
+let boolArr:Array<boolean>
+
+
+let strNumTuple:[string,number]
+
+
+
+myString="Hello TS";
+myNum=2.22;
+myBool=true;
+myVar="yuh";
+
+strNumTuple=["hello",4];
+
+
+
+strArr=["Hello","World"]
+numArr=[1,2,3]
+boolArr=[true,false]
+
+
+let myVoid:void = null;
+let myNull:null=null
+let myUndefined:undefined=undefined
+
+
+
+
+
+console.log(myVoid);
